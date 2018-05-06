8ce71881bdc36cfb1b5334fd2eaebe8e71beb774
kunal13053 <kunal13053@iiitd.ac.in>
2018-05-06 15:23:44 +0530

    MY PART DONE. :)

---
+++ app/src/main/java/e/vishal/wordsuperiorityeffect/Activity2_10.java
@@ -0,0 +1,13 @@
+package e.vishal.wordsuperiorityeffect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity2_10 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity2_10);
+    }
+}
---
+++ app/src/main/java/e/vishal/wordsuperiorityeffect/Activity4_13.java
@@ -0,0 +1,13 @@
+package e.vishal.wordsuperiorityeffect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity4_13 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity4_13);
+    }
+}
